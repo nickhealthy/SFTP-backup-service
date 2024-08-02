42faba1e9b7e7e615bdc9880344d93d92ed289c0
nickhealthy <alskadmlcraz1@gmail.com>
2024-08-02 10:33:17 +0900

    test: 스프링 스케줄링 테스트

--- src/main/java/com/finevo/sftpFiles/SftpFilesApplication.java
+++ src/main/java/com/finevo/sftpFiles/SftpFilesApplication.java
@@ -3,10 +3,12 @@ package com.finevo.sftpFiles;
 import com.jcraft.jsch.JSchException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.io.IOException;
 
 @SpringBootApplication
+@EnableScheduling
 public class SftpFilesApplication {
 
     public static void main(String[] args) throws JSchException, IOException {
---
+++ src/main/java/com/finevo/sftpFiles/scheduler/BackupScheduler.java
@@ -0,0 +1,26 @@
+package com.finevo.sftpFiles.scheduler;
+
+import com.finevo.sftpFiles.service.BackupService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class BackupScheduler {
+
+    private final BackupService backupService;
+
+    @Value("${backup.remoteDir}")
+    private String remoteDir;
+
+    @Value("${backup.localDir}")
+    private String localDir;
+
+    //    @Scheduled(cron = "0 0 0 * * ?") // 매일 자정에 실행
+    @Scheduled(fixedDelay = 5000)
+    public void performBackup() {
+        System.out.println("sftpService = " + backupService);
+    }
+}
